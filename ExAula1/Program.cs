853e75fdd68fdebd00a32d797119609283f227ea
Alquieri <guilherme.alquieri@gmail.com>
2024-08-18 19:05:23 -0300

    Ex 1 a 5
    Aula 1

---
+++ ExAula1/Program.cs
@@ -0,0 +1,85 @@
+﻿/*1. Escreva um algoritmo que leia um número digitado pelo usuário e mostre a
+mensagem “Número maior do que 10!”, caso este número seja maior, ou “Número
+menor ou igual a 10!”, caso este número seja menor ou igual.*/
+
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// if (n1 > 10) {
+// Console.WriteLine("Numero maior que 10 !");
+// } else {
+//     Console.WriteLine("Numero menor ou igual a 10 !");
+//     }
+
+/*2.Escreva um algoritmo que leia dois números digitados pelo usuário e exiba o
+resultado da sua soma.*/
+
+// Console.WriteLine("Digite um numero: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Digite um numero: ");
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Resultado da soma: ");
+// Console.WriteLine(n1 + n2);
+
+
+/*3. Escreva um algoritmo que leia os valores de dois números inteiros distintos nas
+variáveis A e B e informe qual deles é o maior. Caso os números sejam iguais informar
+ao usuário que a sequência de números informados é inválida*/
+
+// Console.WriteLine("Digite um numero [A]: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Digite um numero [B]: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// if (A > B){
+//     Console.WriteLine("O maior numero e A");
+// }
+// else if (B > A){
+//     Console.WriteLine("O maior numero e B");
+//     }else{ 
+//         Console.WriteLine("A sequencia de numeros informados e invalida");
+//         }
+
+/*4. Escreva um algoritmo que leia dois números e ao final mostre a soma, subtração,
+multiplicação e a divisão dos números lidos*/
+
+
+// Console.WriteLine("Digite um numero: ");
+// Double n1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Digite um numero: ");
+// Double n2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Soma: " + (n1 + n2));
+// Console.WriteLine("Subtracao: " + (n1 - n2));
+// Console.WriteLine("Multiplicacao: " + (n1 * n2));
+// if (n2 != 0) {
+//     Console.WriteLine("Divisao: " + (n1 / n2));
+//     }else{
+//         Console.WriteLine("Nao e possivel dividir por zero");
+//         }
+
+
+/*5. Leia valores nas variáveis A e B, e efetue a troca dos valores de forma que o valor
+da variável A passe a ser o valor da variável B e o valor da variável B passe a ser o
+valor da variável A. Apresentar uma mensagem com o valor original de cada variável
+e outra com os valores trocados*/
+
+
+// Console.WriteLine("Digite um numero [A]: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Digite um numero [B]: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Valor original de A: " + A);
+// Console.WriteLine("Valor original de B: " + B);
+// int men = A;
+//  A = B;
+//  B = men;
+//  Console.WriteLine("Novo valor de A: " + A);
+// Console.WriteLine("Novo valor de de B: " + B);
+
+
+
+
+
+
+
+
